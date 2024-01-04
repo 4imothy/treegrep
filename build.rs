c2ea09dd6fcd95c88058a9fd7b99777f382278c3
Timothy Cronin <tcroniniv@gmail.com>
2024-01-03 22:16:37 -0500

    create completions directory if it doesn't exist

--- build.rs
+++ build.rs
@@ -3,19 +3,28 @@
 use clap::ValueEnum;
 use clap_complete::{generate_to, Shell};
 use std::env;
+use std::fs::create_dir_all;
 use std::io::Error;
+use std::path::Path;
 
 include!("src/args.rs");
 
 fn main() -> Result<(), Error> {
-    let _outdir = match env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
-    };
+    let project_root = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let completions_dir_path = Path::new(&project_root).join("completions");
+    if !completions_dir_path.exists() {
+        match create_dir_all(&completions_dir_path) {
+            Ok(()) => {}
+            Err(e) => {
+                panic!("{}", e.to_string());
+            }
+        }
+    }
 
     let mut cmd = generate_command();
     for &shell in Shell::value_variants() {
-        generate_to(shell, &mut cmd, names::BIN_NAME, "completions")?;
+        generate_to(shell, &mut cmd, names::BIN_NAME, &completions_dir_path)?;
     }
 
     Ok(())
