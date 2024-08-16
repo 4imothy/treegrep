a8050bad189aceb9c618474fd843f51d3551a4a0
Timothy Cronin <tcroniniv@gmail.com>
2024-08-15 20:50:56 -0400

    add log system

---
+++ src/log.rs
@@ -0,0 +1,29 @@
+use std::fs;
+use std::io::Write;
+use std::panic;
+use std::sync::{Mutex, OnceLock};
+
+pub static LOG: OnceLock<Mutex<fs::File>> = OnceLock::new();
+
+pub fn gen_log() -> Mutex<fs::File> {
+    Mutex::new(fs::File::create("log").unwrap())
+}
+
+#[allow(unused_macros)]
+macro_rules! log {
+    ($($arg:tt)*) => {
+        writeln!($crate::log::LOG.get_or_init(||
+                $crate::log::gen_log()).lock().unwrap(),
+                $($arg)*).unwrap();
+    };
+}
+
+pub fn set_panic_hook() {
+    panic::set_hook(Box::new(|info| {
+        let log_file = LOG.get_or_init(|| gen_log());
+        writeln!(log_file.lock().unwrap(), "{}", info).unwrap();
+    }));
+}
+
+#[allow(unused_imports)]
+pub(crate) use log;
